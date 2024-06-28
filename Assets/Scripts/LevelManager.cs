ab9b05d2f1c9b4235cc465e731af6d13a9acca8a
Pad <145267541+PadjokeJ@users.noreply.github.com>
2024-06-28 11:22:05 +0200

    Vertical corridors + broken up in segments

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -12,8 +12,10 @@ public class LevelManager : MonoBehaviour
 
     List<GameObject> rooms = new List<GameObject>();
 
-    List<Vector3> corridorsStartPos = new List<Vector3>();
-    List<Vector3> corridorsEndPos = new List<Vector3>();
+    List<Vector3> corridorsRightPos = new List<Vector3>();
+    List<Vector3> corridorsLeftPos = new List<Vector3>();
+    List<Vector3> corridorsUpPos = new List<Vector3>();
+    List<Vector3> corridorsDownPos = new List<Vector3>();
     List<Vector3> corridorsVector = new List<Vector3>();
 
     void Awake()
@@ -35,8 +37,8 @@ public class LevelManager : MonoBehaviour
     {
         float timeToGenerate = Time.realtimeSinceStartup;
         //place rooms in a grid
-        corridorsStartPos.Clear();
-        corridorsEndPos.Clear();
+        corridorsRightPos.Clear();
+        corridorsLeftPos.Clear();
         DestroyDungeon();
 
         //calculate grid dimensions (prioritise squares)
@@ -46,6 +48,8 @@ public class LevelManager : MonoBehaviour
         float roomY = 0;
 
         int row = 0, height = 0;
+        int maxHeight;
+        maxHeight = Mathf.FloorToInt(roomAmmount / gridSize);
 
         LevelObject randomRoom;
         GameObject generatedRoom;
@@ -77,9 +81,19 @@ public class LevelManager : MonoBehaviour
                 int rightIndex = randomRoom.roomDoorsDirection.IndexOf(new Vector2Int(1, 0));
 
                 if (row != 0)
-                    corridorsEndPos.Add(generatedRoom.GetComponent<ListOfDoors>().doors[leftIndex].transform.position);
+                    corridorsLeftPos.Add(generatedRoom.GetComponent<ListOfDoors>().doors[leftIndex].transform.position);
                 if (row < gridSize - 1)
-                    corridorsStartPos.Add(generatedRoom.GetComponent<ListOfDoors>().doors[rightIndex].transform.position);
+                    corridorsRightPos.Add(generatedRoom.GetComponent<ListOfDoors>().doors[rightIndex].transform.position);
+
+
+                int downIndex = randomRoom.roomDoorsDirection.IndexOf(new Vector2Int(0, -1));
+                int upIndex = randomRoom.roomDoorsDirection.IndexOf(new Vector2Int(0, 1));
+
+                if (height != 0)
+                    corridorsDownPos.Add(generatedRoom.GetComponent<ListOfDoors>().doors[downIndex].transform.position);
+                if (height != maxHeight)
+                    corridorsUpPos.Add(generatedRoom.GetComponent<ListOfDoors>().doors[upIndex].transform.position);
+
 
                 if (roomsGenerated >= roomAmmount) break;
                 row++;
@@ -102,9 +116,34 @@ public class LevelManager : MonoBehaviour
     {
         Gizmos.color = Color.white;
 
-        for (int i = 0; i < corridorsStartPos.Count; i++)
+        for (int i = 0; i < corridorsRightPos.Count; i++)
         {
-            Gizmos.DrawLine(corridorsStartPos[i], corridorsEndPos[i]);
+            Vector3 inBetweenRight;
+            inBetweenRight = (corridorsRightPos[i] - corridorsLeftPos[i]) / 2 + corridorsLeftPos[i];
+            inBetweenRight = new Vector3(inBetweenRight.x, corridorsLeftPos[i].y);
+            Gizmos.DrawLine(corridorsLeftPos[i], inBetweenRight);
+
+            Vector3 inBetweenLeft;
+            inBetweenLeft = (corridorsLeftPos[i] - corridorsRightPos[i]) / 2 + corridorsRightPos[i];
+            inBetweenLeft = new Vector3(inBetweenLeft.x, corridorsRightPos[i].y);
+            Gizmos.DrawLine(corridorsRightPos[i], inBetweenLeft);
+
+            Gizmos.DrawLine(inBetweenRight, inBetweenLeft);
+            
+        }
+        for (int i = 0; i < corridorsDownPos.Count; i++)
+        {
+            Vector3 inBetweenUp;
+            inBetweenUp = (corridorsUpPos[i] - corridorsDownPos[i]) / 2 + corridorsDownPos[i];
+            inBetweenUp = new Vector3(corridorsDownPos[i].x, inBetweenUp.y);
+            Gizmos.DrawLine(corridorsDownPos[i], inBetweenUp);
+
+            Vector3 inBetweenDown;
+            inBetweenDown = (corridorsDownPos[i] - corridorsUpPos[i]) / 2 + corridorsUpPos[i];
+            inBetweenDown = new Vector3(corridorsUpPos[i].x, inBetweenDown.y);
+            Gizmos.DrawLine(corridorsUpPos[i], inBetweenDown);
+
+            Gizmos.DrawLine(inBetweenUp, inBetweenDown);
         }
     }
 
@@ -128,8 +167,8 @@ public class LevelManager : MonoBehaviour
         if (destroy)
         {
             DestroyDungeon();
-            corridorsEndPos.Clear();
-            corridorsStartPos.Clear();
+            corridorsLeftPos.Clear();
+            corridorsRightPos.Clear();
         }
         generate = false;
         destroy = false;

632b55779dfa7219d10e2bc91f8b2026ab1ee271
Pad <145267541+PadjokeJ@users.noreply.github.com>
2024-06-29 11:08:08 +0200

    made new function for better edition of corridor placement

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -110,17 +110,27 @@ public class LevelManager : MonoBehaviour
 
 
         //time to generate corridors!
-        tilemapObject = new GameObject();
-        tilemapObject.AddComponent<Tilemap>();
-        tilemapObject.AddComponent<TilemapRenderer>();
-        tilemapObject.AddComponent<Grid>();
-        tilemapObject.AddComponent<TilemapCollider2D>();
 
-        tilemapObject = Instantiate<GameObject>(tilemapObject, Vector3.zero, Quaternion.identity);
-        tilemapObject.name = "Corridor tilemap";
+        tilemapObject = GenerateCorridors(generatedRoom);
+
+        timeToGenerate = Time.realtimeSinceStartup - timeToGenerate;
+        Debug.Log("Dungeon took " + timeToGenerate.ToString() + " seconds to generate");
+
+    }
+
+    GameObject GenerateCorridors(GameObject baseTilemapRoom)
+    {
+        GameObject obj = new GameObject();
+        obj.AddComponent<Tilemap>();
+        obj.AddComponent<TilemapRenderer>();
+        obj.AddComponent<Grid>();
+        obj.AddComponent<TilemapCollider2D>();
+
+        obj = Instantiate<GameObject>(obj, Vector3.zero, Quaternion.identity);
+        obj.name = "Corridor tilemap";
 
         Tile wallTile = new Tile();
-        Tilemap tilemap = generatedRoom.GetComponentInChildren<Tilemap>();
+        Tilemap tilemap = baseTilemapRoom.GetComponentInChildren<Tilemap>();
 
 
         // get the type of tile to use for walls
@@ -133,24 +143,46 @@ public class LevelManager : MonoBehaviour
             }
         }
 
-        tilemap = tilemapObject.GetComponent<Tilemap>();
+        tilemap = obj.GetComponent<Tilemap>();
 
-        foreach(Vector3 position in corridorsRightPos)
-        {
-            tilemap.SetTile(Vector3Int.FloorToInt(position) + new Vector3Int(0, 1), wallTile);
-            tilemap.SetTile(Vector3Int.FloorToInt(position) + new Vector3Int(0, -2), wallTile);
-        }
-        foreach (Vector3 position in corridorsLeftPos)
-        {
-            tilemap.SetTile(Vector3Int.FloorToInt(position) + new Vector3Int(-1, 1), wallTile);
-            tilemap.SetTile(Vector3Int.FloorToInt(position) + new Vector3Int(-1, -2), wallTile);
-        }
+        // break up corridors
+        Vector3 startPos, endPos, midPos1, midPos2;
+        
 
-        timeToGenerate = Time.realtimeSinceStartup - timeToGenerate;
-        Debug.Log("Dungeon took " + timeToGenerate.ToString() + " seconds to generate");
+        for (int i = 0; i < corridorsRightPos.Count; i++)
+        {
+            endPos = corridorsLeftPos[i];
+            startPos = corridorsRightPos[i];
+            midPos1 = (endPos - startPos) / 2 + startPos;
+            midPos1 = new Vector3(midPos1.x, startPos.y);
+
+            midPos2 = (startPos - endPos) / 2 + endPos;
+            midPos2 = new Vector3(midPos2.x, endPos.y);
+
+            SetTiles(wallTile, tilemap, startPos, midPos1);
+            SetTiles(wallTile, tilemap, midPos1, midPos2);
+            SetTiles(wallTile, tilemap, midPos2, endPos);
+        }
+        
+
+        return tilemapObject;
 
     }
-    
+    void SetTiles(Tile wallTile, Tilemap tilemap, Vector3 startPos, Vector3 endPos)
+    {
+        Vector3 segment;
+        segment = endPos - startPos;
+
+        Vector3 displacement;
+        displacement = new Vector3(segment.y, segment.x);
+        displacement.Normalize();
+
+        for (int j = 0; j < Mathf.CeilToInt(segment.magnitude); j++)
+        {
+            tilemap.SetTile(Vector3Int.FloorToInt(startPos + segment.normalized * j + displacement), wallTile);
+        }
+    }
+
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.white;
@@ -197,6 +229,9 @@ public class LevelManager : MonoBehaviour
         corridorsLeftPos.Clear();
         corridorsUpPos.Clear();
         corridorsDownPos.Clear();
+
+        if(tilemapObject != null)
+            tilemapObject.GetComponentInChildren<Tilemap>().ClearAllTiles();
 
         Destroy(tilemapObject);
     }

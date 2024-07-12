82838703e3c7530da3fca8a50e7fb217830ac083
Pad <145267541+PadjokeJ@users.noreply.github.com>
2024-07-12 19:50:51 +0200

    smooth selectionnnnnnnnnnnnnn

--- Assets/Scripts/UiInventoryManager.cs
+++ Assets/Scripts/UiInventoryManager.cs
@@ -8,33 +8,48 @@ public class UiInventoryManager : MonoBehaviour
     PlayerInventory playerInventory;
     public GameObject weaponSlotPrefab;
     List<GameObject> weaponSlots;
+    List<RectTransform> transforms = new List<RectTransform>();
+    int selectedSlot;
 
     public float selectedHeight;
     public float normalHeight;
     public float spacing;
+
+    float offset;
     void Awake()
     {
         playerInventory = FindAnyObjectByType<PlayerInventory>();
         weaponSlots = new List<GameObject>();
 
+        offset = 0.5f * spacing * playerInventory.weapons.Count;
+
         int iteration = 0;
         foreach(GameObject weapon in playerInventory.weapons)
         {
             weaponSlots.Add(Instantiate<GameObject>(weaponSlotPrefab, transform));
-            weaponSlots[iteration].GetComponent<RectTransform>().anchoredPosition += new Vector2(iteration * spacing - 0.5f * spacing * playerInventory.weapons.Count, normalHeight);
+            transforms.Add(weaponSlots[iteration].GetComponent<RectTransform>());
+            transforms[iteration].anchoredPosition = new Vector2(iteration * spacing - offset, normalHeight);
             weaponSlots[iteration].transform.GetChild(0).GetComponent<Image>().sprite = playerInventory.weapons[iteration].GetComponentInChildren<SpriteRenderer>().sprite;
             iteration++;
         }
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        int iteration = 0;
+        foreach(RectTransform slot in transforms)
+        {
+            if (iteration != selectedSlot)
+                slot.anchoredPosition = Vector2.Lerp(slot.anchoredPosition, new Vector2(iteration * spacing - offset, normalHeight), 0.5f);
+            else
+                slot.anchoredPosition = Vector2.Lerp(slot.anchoredPosition, new Vector2(iteration * spacing - offset, selectedHeight), 0.5f);
+            iteration++;
+        }
     }
 
     public void SelectWeapon(int index)
     {
-
+        selectedSlot = index;
     }
 }

f960012bacce77a3b31dae4282194605dd766289
Pad <145267541+PadjokeJ@users.noreply.github.com>
2024-09-16 13:35:59 +0200

    Card sounds when Highlighted

--- Assets/Scripts/Ui/Cards.cs
+++ Assets/Scripts/Ui/Cards.cs
@@ -1,14 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class Cards : MonoBehaviour
+//Use the Selectable class as a base class to access the IsHighlighted method
+public class Cards : Selectable
 {
     public Image buffSprite;
     public TextMeshProUGUI buffHeadline;
     public TextMeshProUGUI buffDescription;
 
     public BuffScriptableObject buff;
+    //Use this to check what Events are happening
+    BaseEventData m_BaseEvent;
+
+    bool isHighlighted;
+
+    public AudioClip cardReveal;
+
+    void Update()
+    {
+        if (IsHighlighted() && isHighlighted != IsHighlighted())
+            OnHighlight();
+        isHighlighted = IsHighlighted();
+    }
+
+    void OnHighlight()
+    {
+        Debug.Log("highlited this!!!");
+        AudioManager.instance.PlayAudio(cardReveal, transform.position, 2f, 0.2f);
+    }
 }

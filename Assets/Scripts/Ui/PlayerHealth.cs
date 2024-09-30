1bfce2c1ea6fdb8195873285edbd8db0c58a1818
Pad <145267541+PadjokeJ@users.noreply.github.com>
2024-09-30 16:08:45 +0200

    try making HP more comprehensible

--- Assets/Scripts/Ui/PlayerHealth.cs
+++ Assets/Scripts/Ui/PlayerHealth.cs
@@ -3,19 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+using TMPro;
+
 public class PlayerHealth : MonoBehaviour
 {
     Health playerHealth;
     Image healthBar;
+
+    TextMeshProUGUI healthText;
+
+
     void Awake()
     {
         playerHealth = FindAnyObjectByType<PlayerManager>().GetComponent<Health>();
         healthBar = GetComponent<Image>();
+
+        healthText = transform.GetChild(0).GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        healthBar.fillAmount = Mathf.RoundToInt(((0.64f * playerHealth.health / playerHealth.maxHealth) + .18f) * 16f) / 16f;
+        healthBar.fillAmount = Mathf.CeilToInt(((0.64f * playerHealth.health / playerHealth.maxHealth) + .18f) * 16f) / 16f;
+        healthText.text = $"{playerHealth.health}/{playerHealth.maxHealth}";
     }
 }
